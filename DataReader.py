865d1bf48efd92320a39bc881294fdbe77df2b07
AlexFxw <alex93179@gmail.com>
2018-12-12 12:01:20 +0800

    Modify the DataReader.py

--- DataReader.py
+++ DataReader.py
@@ -1,41 +1,31 @@
 #!/usr/bin/python3
 import pymongo as mongo
-import time
+import datetime as datetime
 
 
 class DataReader:
     def __init__(self):
         self.client = mongo.MongoClient("mongodb://101.6.114.5:27017")
         self.database = self.client["AutoID"]
+        #self.database = self.client["test"]
         self.tagData = self.database["TagData"]
         self.timeFormat = "%Y-%m-%dT%H:%M:%S"
         self.timeDelta = 28800  # Beijing timezone is 8 hours faster than UTC
 
-    def GetData(self, startTime, endTime, count=100):
-        start = int(time.mktime(time.strptime(startTime, self.timeFormat))) - self.timeDelta
-        end = int(time.mktime(time.strptime(endTime, self.timeFormat))) - self.timeDelta
-        found = self.tagData.find()
-        result = []
-        cnt = 0
-        for item in found:
-            timeStr = item["Time"][0:19]
-            timeStamp = int(time.mktime(time.strptime(timeStr, self.timeFormat)))
-            if (timeStamp >= start and timeStamp <= end):
-                # Suppose the time is strictly increasing
-                result.append(item)
-                cnt = cnt + 1
-            elif (timeStamp > end):
-                break
-
-            if (cnt >= count):
-                break
-        return result
+    def GetData(self, startTime, endTime, count=None):
+        found = self.tagData.find({'Time': {'$lt': endTime, '$gte': startTime}})
+        if count is None:
+            return found
+        else:
+            return found[:count] if found.count() > count else found
+    def PKTime(self, year, month, day, hour, minute, sec):
+        return datetime.datetime(year, month, day, hour - 8, minute, sec)
 
 
 if __name__ == '__main__':
     reader = DataReader()
-    startTime = "2018-12-05T13:00:00"
-    endTime = "2018-12-08T17:00:00"
-    result = reader.GetData(startTime, endTime, 100000)
+    startTime = reader.PKTime(2018, 12, 12, 10, 40, 00)
+    endTime = reader.PKTime(2018, 12, 12, 11, 40, 00)
+    result = reader.GetData(startTime, endTime, 100)
     for item in result:
         print(item)
