06909d0f8bcc4951dcc388dae647f3b0c209dc47
AlexFxw <alex93179@gmail.com>
2018-12-12 15:08:01 +0800

    Support filter with epc id. Add argument parser so that users do not need to modify the source code for searching.

--- DataReader.py
+++ DataReader.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import pymongo as mongo
 import datetime as datetime
+import argparse
+import re
 
 
 class DataReader:
@@ -10,22 +12,34 @@ class DataReader:
         #self.database = self.client["test"]
         self.tagData = self.database["TagData"]
         self.timeFormat = "%Y-%m-%dT%H:%M:%S"
-        self.timeDelta = 28800  # Beijing timezone is 8 hours faster than UTC
-
-    def GetData(self, startTime, endTime, count=None):
-        found = self.tagData.find({'Time': {'$lt': endTime, '$gte': startTime}})
+        self.timeDelta = 8  # Beijing timezone is 8 hours faster than UTC
+    def GetData(self, startTime, endTime, count=None, epc=None):
+        if epc is None:
+            found = self.tagData.find({'Time': {'$lt': endTime, '$gte': startTime}})
+        else:
+            found = self.tagData.find({
+                'Time': {'$lt': endTime, '$gte': startTime},
+                'EPC': epc
+            })
         if count is None:
             return found
         else:
             return found[:count] if found.count() > count else found
     def PKTime(self, year, month, day, hour, minute, sec):
-        return datetime.datetime(year, month, day, hour - 8, minute, sec)
-
-
+        return datetime.datetime(year, month, day, hour - self.timeDelta, minute, sec)
+    
 if __name__ == '__main__':
     reader = DataReader()
-    startTime = reader.PKTime(2018, 12, 12, 10, 40, 00)
-    endTime = reader.PKTime(2018, 12, 12, 11, 40, 00)
-    result = reader.GetData(startTime, endTime, 100)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epc", dest = "epc", help = "The EPC of the tag you want to find. Ignore if you want all")
+    parser.add_argument("--num", dest = "num", help = "How many data you want to get?")
+    parser.add_argument("--start", dest = "start", help = "Start time. Format in  yyyy/MM/dd/HH/mm/ss", default = "2018/12/12/10/40/00")
+    parser.add_argument("--end", dest = "end", help = "End time. Format in  yyyy/MM/dd/HH/mm/ss", default = "2018/12/12/11/40/00")
+    args = parser.parse_args()
+    startOpt = re.split('/', args.start) 
+    startTime = reader.PKTime(int(startOpt[0]),int(startOpt[1]),int(startOpt[2]),int(startOpt[3]),int(startOpt[4]),int(startOpt[5]))
+    endOpt = re.split('/', args.end) 
+    endTime = reader.PKTime(int(endOpt[0]),int(endOpt[1]),int(endOpt[2]),int(endOpt[3]),int(endOpt[4]),int(endOpt[5]))
+    result = reader.GetData(startTime, endTime, args.num, args.epc)
     for item in result:
         print(item)
--- README.md
+++ README.md
@@ -5,3 +5,19 @@ The deep learning codes for AutoID project
 ### MongoDB
 The configure file on Windows server should set bindIP to 0.0.0.0 (listen to all IP) or specific network interface.
 Use `pip3 install pymongo` to install in the python virtual environment.
+
+## Usage
+### DataReader
+#### import as a module
+``` python
+reader = DataReader()
+startTime = reader.PKTime(2018, 10, 10, 22, 30, 00) #2018/10/10 22:30 0s
+endTime = reader.PKTime(2018, 12, 31, 23, 30, 30)
+epc = "E200001939070049131014D4"
+result = reader.GetData(startTime, endTime, 100, epc) # Tag with id epc's first 100 records in this interval
+result = reader.GetData(startTime, endTime, 100) # 100 records from all tags in this interval
+result = reader.GetData(startTime, endTime, None, epc) # Tag with id epc's all records in this interval
+result = reader.GetData(startTime, endTime) # All records from all tags in this interval
+```
+#### Usage in terminal
+`python3 DataReader.py -h` will give your help. Remember to launch correct virtual environment first.
