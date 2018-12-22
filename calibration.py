1d47c7110a9086ddd060f0c0d3bc381155ee7c8f
qianzgit <qianz_need@163.com>
2018-12-23 01:30:33 +0800

    每个标签使用最小的频率902750作为基准频率，使用线性回归计算该频率的

--- calibration.py
+++ calibration.py
@@ -5,6 +5,26 @@ import argparse
 import sys
 import csv
 
+def fit(x,y):
+    if len(x) != len(y):
+        return
+    numerator = 0.0
+    denominator = 0.0
+    x_mean = np.mean(x)
+    y_mean = np.mean(y)
+    for i in range(len(x)):
+        numerator += (x[i]-x_mean)*(y[i]-y_mean)
+        denominator += np.square((x[i]-x_mean))
+    print('numerator:',numerator,'denominator:',denominator)
+    b0 = numerator/denominator
+    b1 = y_mean - b0*x_mean
+    return b0,b1
+
+
+# 定义预测函数
+def predit(x,b0,b1):
+    return b0*x + b1
+
 # to calculate the var
 def phase_calibrate(inputfile,outputfile):
     file = inputfile
@@ -56,10 +76,36 @@ def phase_calibrate(inputfile,outputfile):
                         if every < meantmp:
                             frequency_dic[key] += 180
                 average_dic[key] = frequency_dic[key] / number_dic[key]
-            minfre =  pd.Series(frelist).min()
-            if frelist:
+            if frelist: 
+                #求基准频率
+                frequency_array=[]
+                average_array=[]
+                for key in frequency_dic:
+                    frequency_array.append(int(key))
+                frequency_array.sort()
+                for key in frequency_array:
+                    average_array.append(average_dic[str(key)])
+                length = len(average_array)
+                # 如果只有一个频率，那么无法通过线性回归求取基准频率，为了防止产生nan，直接取原值，需要注意
+                if length == 1:
+                    fr = average_array[0]
+                else :
+                    base = 0
+                    average_array_after=[]
+                    average_array_after.append(average_array[0])
+                    for tmp_i in range(1,length):
+                        if average_array[tmp_i] - average_array[tmp_i-1] > 0:
+                            base += 180
+                            average_array_after.append(average_array[tmp_i]-base)
+                        else:
+                            average_array_after.append(average_array[tmp_i]-base)
+                    b0,b1=fit(frequency_array,average_array_after)
+                    fr = predit(902750,b0,b1)
+                    while fr > 180:
+                        fr -= 180
                 print(frelist)
-                phi_r = average_dic[str(minfre)]
+                # phi_r = average_dic[str(minfre)]
+                phi_r = fr
                 with open(file) as csvfile:
                     csv_reader = csv.reader(csvfile)
                     headers = next(csv_reader)
