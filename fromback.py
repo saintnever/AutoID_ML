7b06ef84e516a05c8f6d5dcd9f5d7243e9ca68f1
qianzgit <qianz_need@163.com>
2019-03-27 17:19:32 +0800

    change the usage of the function in fromback.py

--- fromback.py
+++ fromback.py
@@ -8,6 +8,7 @@ from button_bitid import Button
 import csv
 
 xInput = {}
+
 xInput['EPC']=[]
 xInput['Antenna'] = []
 xInput['Freq']=[]
@@ -17,10 +18,14 @@ xInput['Doppler']=[]
 xInput['Phase']=[]
 xInput['ComputerTimestamp']=[]
 index = 0
-# old button
-# button = Button('E2000019390701021310458F')
-# new button
-button = Button('E2000019390700211300052E')
+
+# 存放EPC
+EPClist=[]
+
+# 存放状态
+resultlist = []
+
+
 def receivedata():
     s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.connect(('101.6.114.22',14))
@@ -66,6 +71,14 @@ def receivedata():
             break
         # print(xInput)
 
+def updateEPC(xEPClist):
+    global EPClist
+    EPClist = xEPClist
+
+def getresult():
+    global resultlist
+    return resultlist
+
 def lower_bound(array,first,last,value):
     while first < last:
         mid =  first + (last - first) // 2
@@ -74,39 +87,44 @@ def lower_bound(array,first,last,value):
             last = mid
     return first
 
-def processdata(EPClist):
+def processdata():
     global xInput
     global index
-    buttonlist = []
-    for item in EPClist:
-        tempbutton = Button(item)
-        buttonlist.append(tempbutton)
+    global EPClist
+    global resultlist
     timeEnd = timedelta(seconds = 0)
     win = timedelta(seconds=0.2)
     step = timedelta(seconds=0.1)
     resultlist = []
-    # while True:
-    if len(xInput['ReaderTimestamp']):
-        while True:
-            temp = max(xInput['ReaderTimestamp'])
-            # 考虑两个时间节点，一个是按照step走的timeEnd，还有一个是现在接收到的数据的最迟时间
-            if  temp > timeEnd:
-                timeEnd = temp
-                break
-            else:
-                time.sleep(0.1)
-        timeStart = timeEnd - win
-        index  = lower_bound(xInput['ReaderTimestamp'],index,len(xInput['ReaderTimestamp']),timeStart)
-        for item in buttonlist:
-            resultlist.append(item.winstatusChangelist(xInput,index))
-        # print(button.winstatusChangelist(xInput,index))
+    while True:
+        if len(xInput['ReaderTimestamp']):
+            while True:
+                temp = max(xInput['ReaderTimestamp'])
+                # 考虑两个时间节点，一个是按照step走的timeEnd，还有一个是现在接收到的数据的最迟时间
+                if  temp > timeEnd:
+                    timeEnd = temp
+                    break
+                else:
+                    time.sleep(0.1)
+            timeStart = timeEnd - win
+            index  = lower_bound(xInput['ReaderTimestamp'],index,len(xInput['ReaderTimestamp']),timeStart)
+            buttonlist = []
+            for item in EPClist:
+                tempbutton = Button(item)
+                buttonlist.append(tempbutton)
+            resultlist = []
+            for item in buttonlist:
+                resultlist.append(item.winstatusChangelist(xInput,index))
         timeEnd = timeEnd+step
-    return resultlist
+
 
 # receivedata()
 
-# t1 = threading.Thread(target=receivedata, args=())
-# t2 = threading.Thread(target=processdata, args=())
+# receivedata
+t1 = threading.Thread(target=receivedata, args=())
+# processdata
+t2 = threading.Thread(target=processdata, args=())
+
 # t1.start()
 # t2.start()
 # t1.join()
